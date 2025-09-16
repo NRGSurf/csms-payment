77073931be49aaad5c38ac1d5d00330ff714d3ff
Stefan Rom <stefanrom92@googlemail.com>
2025-09-16 14:33:57 +0200

    update to prod

--- next.config.ts
+++ next.config.ts
@@ -16,7 +16,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/data/:path*", // browser will call https://your-app.vercel.app/data/...
-        destination: "http://161.35.23.7:8080/data/:path*", // server-side forward
+        destination: "http://134.122.66.91:8080/data/:path*", // server-side forward
       },
     ];
   },
